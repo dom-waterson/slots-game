ba6c97dc5bf757feba02e1a2f4420d4a14a417e0
Dominic Waterson <dom_waterson@hotmail.com>
2018-08-14 22:40:56 +0100

    added css minification to production webpack build

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,7 +1,21 @@
-const merge = require('webpack-merge'); // eslint-disable-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
+const merge = require('webpack-merge');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+/* eslint-disable-line import/no-extraneous-dependencies */
 const baseConfig = require('./webpack.base.config.js');
 
 module.exports = merge(baseConfig, {
   mode: 'production',
   devtool: 'source-map',
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
 });
